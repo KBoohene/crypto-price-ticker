b1327641d3591692989a85ff8eca327fa1b241aa
Kwabena Boohene <okwabse@gmail.com>
2018-04-04 15:34:55 +0000

    Testing github link to heroku

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>Prices</h1>
         <p>BTC: {this.state.BTC}</p>
         <p>ETH: {this.state.ETH}</p>
         <p>LTC: {this.state.LTC}</p>
