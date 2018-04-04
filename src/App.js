bf418db4d8797b8646c6c28087b0f9cff26aa853
Kwabena Boohene <okwabse@gmail.com>
2018-04-04 14:58:05 +0000

    Initialized component did mount function to contain xhr response

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from './api'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount(){
+    api.mainApp((data)=>{
+      console.log(data)
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +23,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
