6ef220ee195d5f8a781ee9594b56c6827db52016
Kwabena Boohene <okwabse@gmail.com>
2018-04-07 09:37:05 +0000

    Added navigation header to page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import api from './api'
 import 'semantic-ui-css/semantic.min.css';
-import { Grid, Divider, Header, Container } from 'semantic-ui-react'
+import { Grid, Divider, Header, Container, Menu, Input } from 'semantic-ui-react'
 
 class App extends Component {
   constructor(props){
@@ -11,28 +11,40 @@ class App extends Component {
       'BTC':'',    
       'ETH':'',
       'LTC':'',
-      'XRP':''
+      'XRP':'',
+      'activeItem':'home',
     }
   }
   
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   //Constant update of crypto prices
   componentDidMount(){
     api.mainApp((data)=>{
-      this.setState({"BTC":data['BTC']['USD']})
-      this.setState({"ETH":data['ETH']['USD']})
-      this.setState({"LTC":data['LTC']['USD']})
-      this.setState({"XRP":data['XRP']['USD']})
+      this.setState({"BTC":data['BTC']['USD']});
+      this.setState({"ETH":data['ETH']['USD']});
+      this.setState({"LTC":data['LTC']['USD']});
+      this.setState({"XRP":data['XRP']['USD']});
     })
   }
   render() {
+    const { activeItem } = this.state;
     return (
       <div className="container">
+        <Menu secondary>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Input icon='search' placeholder='Search...' />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
         <Container>
           <Header as='h1' textAlign='center'>
             First Header
           </Header>
           <Divider />
-          <Grid columns={2} celled>
+          <Grid columns={4} celled>
             <Grid.Row>
               <Grid.Column>
                 BTC: ${this.state.BTC}
@@ -40,13 +52,25 @@ class App extends Component {
               <Grid.Column>
                 ETH: ${this.state.ETH}
               </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
               <Grid.Column>
                 LTC: ${this.state.LTC}
               </Grid.Column>
               <Grid.Column>
                 XRP: ${this.state.XRP}
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+            <Grid.Column>
+                Dash: $
+              </Grid.Column>
+              <Grid.Column>
+                NEO: $
+              </Grid.Column>
+              <Grid.Column>
+                BCH: $
+              </Grid.Column>
+              <Grid.Column>
+                EOS: $
               </Grid.Column>
             </Grid.Row>
           </Grid>
