7167b628e189d556b78c24bae3aa3c0cbb4c4c68
Kwabena Boohene <okwabse@gmail.com>
2018-04-10 10:59:05 +0000

    Added final four crypto currencies

--- src/App.js
+++ src/App.js
@@ -1,52 +1,71 @@
-import React, { Component } from 'react';
-import './App.css';
-import api from './api'
-import 'semantic-ui-css/semantic.min.css';
-import { Grid, Divider, Header, Container, Menu, Input, Card } from 'semantic-ui-react'
+import React, { Component } from "react";
+import "./App.css";
+import api from "./api";
+import "semantic-ui-css/semantic.min.css";
+import {
+  Grid,
+  Divider,
+  Header,
+  Container,
+  Menu,
+  Input,
+  Card
+} from "semantic-ui-react";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      'BTC':'',    
-      'ETH':'',
-      'LTC':'',
-      'XRP':'',
-      'DASH':'',
-      'EOS':'',
-      'NEO':'',
-      'BCH':'',
-      'activeItem':'home',
-    }
+      BTC: "",
+      ETH: "",
+      LTC: "",
+      XRP: "",
+      DASH: "",
+      EOS: "",
+      NEO: "",
+      BCH: "",
+      activeItem: "home"
+    };
   }
-  
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   //Constant update of crypto prices
-  //TODO: Set state for the remainder of the cryptos
-  componentDidMount(){
-    api.mainApp((data)=>{
-      this.setState({"BTC":data['BTC']['USD']});
-      this.setState({"ETH":data['ETH']['USD']});
-      this.setState({"LTC":data['LTC']['USD']});
-      this.setState({"XRP":data['XRP']['USD']});
-    })
+  componentDidMount() {
+    api.mainApp(data => {
+      this.setState({ BTC: data["BTC"]["USD"] });
+      this.setState({ ETH: data["ETH"]["USD"] });
+      this.setState({ LTC: data["LTC"]["USD"] });
+      this.setState({ XRP: data["XRP"]["USD"] });
+      this.setState({ BCH: data["BCH"]["USD"] });
+      this.setState({ NEO: data["NEO"]["USD"] });
+      this.setState({ DASH: data["DASH"]["USD"] });
+      this.setState({ EOS: data["EOS"]["USD"] });
+    });
   }
   render() {
     const { activeItem } = this.state;
     return (
       <div className="container">
         <Menu secondary>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
-          <Menu.Menu position='right'>
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="about"
+            active={activeItem === "about"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Menu position="right">
             <Menu.Item>
-              <Input icon='search' placeholder='Search...' />
+              <Input icon="search" placeholder="Search..." />
             </Menu.Item>
           </Menu.Menu>
         </Menu>
         <Container>
-          <Header as='h1' textAlign='center'>
+          <Header as="h1" textAlign="center">
             Top 8 Cryptos
           </Header>
           <Divider />
@@ -54,60 +73,44 @@ class App extends Component {
             <Grid.Row>
               <Grid.Column>
                 <Card fluid raised>
-                  <Card.Content>
-                    BTC: ${this.state.BTC}
-                  </Card.Content>
+                  <Card.Content>BTC: ${this.state.BTC}</Card.Content>
                 </Card>
               </Grid.Column>
               <Grid.Column>
                 <Card fluid raised>
-                  <Card.Content>
-                    ETH: ${this.state.ETH}
-                  </Card.Content>
+                  <Card.Content>ETH: ${this.state.ETH}</Card.Content>
                 </Card>
               </Grid.Column>
               <Grid.Column>
                 <Card fluid raised>
-                  <Card.Content>
-                    LTC: ${this.state.LTC}
-                  </Card.Content>
+                  <Card.Content>LTC: ${this.state.LTC}</Card.Content>
                 </Card>
               </Grid.Column>
               <Grid.Column>
                 <Card fluid raised>
-                  <Card.Content>
-                    XRP: ${this.state.XRP}
-                  </Card.Content>
+                  <Card.Content>XRP: ${this.state.XRP}</Card.Content>
                 </Card>
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column>
                 <Card fluid raised>
-                  <Card.Content>
-                    DASH: ${this.state.DASH}
-                  </Card.Content>
+                  <Card.Content>DASH: ${this.state.DASH}</Card.Content>
                 </Card>
               </Grid.Column>
               <Grid.Column>
                 <Card fluid raised>
-                  <Card.Content>
-                    NEO: ${this.state.NEO}
-                  </Card.Content>
+                  <Card.Content>NEO: ${this.state.NEO}</Card.Content>
                 </Card>
               </Grid.Column>
               <Grid.Column>
                 <Card fluid raised>
-                  <Card.Content>
-                    BCH: ${this.state.BCH}
-                  </Card.Content>
+                  <Card.Content>BCH: ${this.state.BCH}</Card.Content>
                 </Card>
               </Grid.Column>
-              <Grid.Column >
+              <Grid.Column>
                 <Card fluid raised>
-                  <Card.Content>
-                    EOS: ${this.state.EOS}
-                  </Card.Content>
+                  <Card.Content>EOS: ${this.state.EOS}</Card.Content>
                 </Card>
               </Grid.Column>
             </Grid.Row>
--- src/api.js
+++ src/api.js
@@ -5,20 +5,23 @@
 let xhr = new XMLHttpRequest();
 
 const api_func = {
-  //TODO: Get prices for EOS, DASH, BCH, NEO 
+  //TODO: Get prices for EOS, DASH, BCH, NEO
   //Async function to retrieve crypto prices
   mainApp(data) {
-    setInterval(function(){
-      xhr.open('GET', 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP&tsyms=USD', true)
+    setInterval(function() {
+      xhr.open(
+        "GET",
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP,BCH,EOS,NEO,DASH&tsyms=USD",
+        true
+      );
       xhr.send();
       xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
           data(JSON.parse(xhr.response));
         }
-      }
-    }, 10000)
-  },
-
-}
+      };
+    }, 10000);
+  }
+};
 
 export default api_func;
