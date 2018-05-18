39bf546713351a4a4e4519db63b6dc326987bdf2
Kwabena Boohene <okwabse@gmail.com>
2018-05-18 17:05:24 +0000

    Testing cyfa-beta-api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import "./App.css";
 import api from "./api";
 import "semantic-ui-css/semantic.min.css";
@@ -11,7 +13,9 @@ import {
   Input,
   Card
 } from "semantic-ui-react";
-import { img_32 } from "./crypto-imgs/img_dir";
+import {
+  img_32
+} from "./crypto-imgs/img_dir";
 const crypto_images = require.context("./crypto-imgs", true);
 
 class App extends Component {
@@ -30,7 +34,9 @@ class App extends Component {
     };
   }
 
-  handleItemClick = (e, { name }) =>
+  handleItemClick = (e, {
+      name
+    }) =>
     this.setState({
       activeItem: name
     });
@@ -65,122 +71,237 @@ class App extends Component {
     });
   }
   render() {
-    const { activeItem } = this.state;
-    return (
-      <div className="container">
-        <Menu secondary>
-          <Menu.Item
-            name="home"
-            active={activeItem === "home"}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name="about"
-            active={activeItem === "about"}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Menu position="right">
-            <Menu.Item>
-              <Input icon="search" placeholder="Search..." />
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
-        <Container>
-          <Header as="h1" textAlign="center">
-            Top 8 Cryptos
-          </Header>
-          <Divider />
-          <Grid columns={4}>
-            <Grid.Row>
-              <Grid.Column>
-                <Card fluid raised>
-                  <Card.Content>
-                    <span style={styles.crypto_imgs}>
-                      <img
-                        alt="btc-32-color"
-                        src={crypto_images(img_32.color.btc)}
-                      />
-                    </span>
-                    <span>BTC: $ {this.state.BTC}</span>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-              <Grid.Column>
-                <Card fluid raised>
-                  <Card.Content>
-                    <span style={styles.crypto_imgs}>
-                      <img alt="" src={crypto_images(img_32.color.eth)} />
-                    </span>
-                    <span>ETH: $ {this.state.ETH}</span>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-              <Grid.Column>
-                <Card fluid raised>
-                  <Card.Content>
-                    <span style={styles.crypto_imgs}>
-                      <img alt="" src={crypto_images(img_32.color.ltc)} />
-                    </span>
-                    <span> LTC: $ {this.state.LTC}</span>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-              <Grid.Column>
-                <Card fluid raised>
-                  <Card.Content>
-                    <span style={styles.crypto_imgs}>
-                      <img alt="" src={crypto_images(img_32.color.xrp)} />
-                    </span>
-                    <span>XRP: $ {this.state.XRP}</span>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column>
-                <Card fluid raised>
-                  <Card.Content>
-                    <span style={styles.crypto_imgs}>
-                      <img alt="" src={crypto_images(img_32.color.dash)} />
-                    </span>
-                    <span>DASH: $ {this.state.DASH}</span>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-              <Grid.Column>
-                <Card fluid raised>
-                  <Card.Content>
-                    <span style={styles.crypto_imgs}>
-                      <img alt="" src={crypto_images(img_32.color.neo)} />
-                    </span>
-                    <span>NEO: $ {this.state.NEO}</span>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-              <Grid.Column>
-                <Card fluid raised>
-                  <Card.Content>
-                    <span style={styles.crypto_imgs}>
-                      <img alt="" src={crypto_images(img_32.color.bch)} />
-                    </span>
-                    <span>BCH: $ {this.state.BCH}</span>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-              <Grid.Column>
-                <Card fluid raised>
-                  <Card.Content>
-                    <span style={styles.crypto_imgs}>
-                      <img alt="" src={crypto_images(img_32.color.eos)} />
-                    </span>
-                    <span>EOS: $ {this.state.EOS}</span>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
-      </div>
+    const {
+      activeItem
+    } = this.state;
+    return ( <
+      div className = "container" >
+      <
+      Menu secondary >
+      <
+      Menu.Item name = "home"
+      active = {
+        activeItem === "home"
+      }
+      onClick = {
+        this.handleItemClick
+      }
+      /> <
+      Menu.Item name = "about"
+      active = {
+        activeItem === "about"
+      }
+      onClick = {
+        this.handleItemClick
+      }
+      /> <
+      Menu.Menu position = "right" >
+      <
+      Menu.Item >
+      <
+      Input icon = "search"
+      placeholder = "Search..." / >
+      <
+      /Menu.Item> < /
+      Menu.Menu > <
+      /Menu> <
+      Container >
+      <
+      Header as = "h1"
+      textAlign = "center" >
+      Top 8 Cryptos <
+      /Header> <
+      Divider / >
+      <
+      Grid columns = {
+        4
+      } >
+      <
+      Grid.Row >
+      <
+      Grid.Column >
+      <
+      Card fluid raised >
+      <
+      Card.Content >
+      <
+      span style = {
+        styles.crypto_imgs
+      } >
+      <
+      img alt = "btc-32-color"
+      src = {
+        crypto_images(img_32.color.btc)
+      }
+      /> < /
+      span > <
+      span > BTC: $ {
+        this.state.BTC
+      } < /span> < /
+      Card.Content > <
+      /Card> < /
+      Grid.Column > <
+      Grid.Column >
+      <
+      Card fluid raised >
+      <
+      Card.Content >
+      <
+      span style = {
+        styles.crypto_imgs
+      } >
+      <
+      img alt = ""
+      src = {
+        crypto_images(img_32.color.eth)
+      }
+      /> < /
+      span > <
+      span > ETH: $ {
+        this.state.ETH
+      } < /span> < /
+      Card.Content > <
+      /Card> < /
+      Grid.Column > <
+      Grid.Column >
+      <
+      Card fluid raised >
+      <
+      Card.Content >
+      <
+      span style = {
+        styles.crypto_imgs
+      } >
+      <
+      img alt = ""
+      src = {
+        crypto_images(img_32.color.ltc)
+      }
+      /> < /
+      span > <
+      span > LTC: $ {
+        this.state.LTC
+      } < /span> < /
+      Card.Content > <
+      /Card> < /
+      Grid.Column > <
+      Grid.Column >
+      <
+      Card fluid raised >
+      <
+      Card.Content >
+      <
+      span style = {
+        styles.crypto_imgs
+      } >
+      <
+      img alt = ""
+      src = {
+        crypto_images(img_32.color.xrp)
+      }
+      /> < /
+      span > <
+      span > XRP: $ {
+        this.state.XRP
+      } < /span> < /
+      Card.Content > <
+      /Card> < /
+      Grid.Column > <
+      /Grid.Row> <
+      Grid.Row >
+      <
+      Grid.Column >
+      <
+      Card fluid raised >
+      <
+      Card.Content >
+      <
+      span style = {
+        styles.crypto_imgs
+      } >
+      <
+      img alt = ""
+      src = {
+        crypto_images(img_32.color.dash)
+      }
+      /> < /
+      span > <
+      span > DASH: $ {
+        this.state.DASH
+      } < /span> < /
+      Card.Content > <
+      /Card> < /
+      Grid.Column > <
+      Grid.Column >
+      <
+      Card fluid raised >
+      <
+      Card.Content >
+      <
+      span style = {
+        styles.crypto_imgs
+      } >
+      <
+      img alt = ""
+      src = {
+        crypto_images(img_32.color.neo)
+      }
+      /> < /
+      span > <
+      span > NEO: $ {
+        this.state.NEO
+      } < /span> < /
+      Card.Content > <
+      /Card> < /
+      Grid.Column > <
+      Grid.Column >
+      <
+      Card fluid raised >
+      <
+      Card.Content >
+      <
+      span style = {
+        styles.crypto_imgs
+      } >
+      <
+      img alt = ""
+      src = {
+        crypto_images(img_32.color.bch)
+      }
+      /> < /
+      span > <
+      span > BCH: $ {
+        this.state.BCH
+      } < /span> < /
+      Card.Content > <
+      /Card> < /
+      Grid.Column > <
+      Grid.Column >
+      <
+      Card fluid raised >
+      <
+      Card.Content >
+      <
+      span style = {
+        styles.crypto_imgs
+      } >
+      <
+      img alt = ""
+      src = {
+        crypto_images(img_32.color.eos)
+      }
+      /> < /
+      span > <
+      span > EOS: $ {
+        this.state.EOS
+      } < /span> < /
+      Card.Content > <
+      /Card> < /
+      Grid.Column > <
+      /Grid.Row> < /
+      Grid > <
+      /Container> < /
+      div >
     );
   }
 }
